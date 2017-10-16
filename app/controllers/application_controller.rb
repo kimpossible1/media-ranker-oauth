f67922676e4557ca10875b6f59035fc7909debf5
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-16 16:30:34 -0700

    completed log in and log out feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,34 @@ class ApplicationController < ActionController::Base
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
+  protect_from_forgery with: :exception
+
+  # before_action :require_login
+
+# protected
+#   def require_login
+#     @user = User.find_by(id: session[:user_id])
+#     unless @user
+#       flash[:status] = :failure
+#       flash[:message] = "You must be logged in to do that!"
+#       redirect_to root_path
+#     end
+#   end
+
+  def save_and_flash(model)
+    result = model.save
+
+    if result
+      flash[:status] = :success
+      flash[:message] = "Successfully saved #{model.class} #{model.id}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "Failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
+    end
+
+    return result
+  end
 
 private
   def find_user
@@ -14,4 +42,6 @@ private
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+skip_before_action :require_login, only: [:login]
+
   def index
     @users = User.all
   end
@@ -7,4 +9,35 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render_404 unless @user
   end
+
+
+
+  def login
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
+      if user.nil?
+        # User has not logged in before
+        # Create a new record in the DB
+        user = User.from_auth_hash(params[:provider], auth_hash)
+        save_and_flash(user)
+
+      else
+        flash[:status] = :success
+        flash[:message] = "Successfully logged in as returning user #{user.name}"
+
+      end
+
+      # Log the user in
+      session[:user_id] = user.id
+
+    else
+      flash[:status] = :failure
+      flash[:message] = "Could not create user from OAuth data"
+    end
+
+    redirect_to root_path
+  end
+  
 end
