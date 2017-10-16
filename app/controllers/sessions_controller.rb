f0f0b27f4ef136a5af4a3937cdebf0ba745a6fa3
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-16 15:19:56 -0700

    working on create method and log in log out buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if user.nil?
         # User doesn't match anything in the DB
         # Attempt to create a new user
-        user = User.build_from_github(auth_hash)
+        user = User.build_from_github(auth_hash[:uid], provider: 'github')
       else
         flash[:success] = "Logged in successfully"
         redirect_to root_path
@@ -59,4 +59,3 @@ end
   #   flash[:result_text] = "Successfully logged out"
   #   redirect_to root_path
   # end
-end
